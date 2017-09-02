b20d78963f859aa4a3cae5c4038888126f98baec
Tc2r <tc2r1@yahoo.com>
2017-09-02 02:26:28 -0500

    Update DownloadUrl.java

--- app/src/main/java/com/tc2r/placesapi/DownloadUrl.java
+++ app/src/main/java/com/tc2r/placesapi/DownloadUrl.java
@@ -12,18 +12,23 @@ import java.net.URL;
 
 public class DownloadUrl {
 
+	// Reads the Url and returns a String holding all the Json Data
 	public String readUrl(String myUrl) {
 		String data = "";
 		InputStream inputStream = null;
 		HttpURLConnection urlConnection = null;
 
 		try {
+
+			// Create a Http Connection
 			URL url = new URL(myUrl);
 			urlConnection = (HttpURLConnection) url.openConnection();
 			urlConnection.connect();
 
+			// Get the input stream.
 			inputStream = urlConnection.getInputStream();
 
+			// Read through the inputstream and create a string.
 			BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
 			StringBuffer sb = new StringBuffer();
 
@@ -53,6 +58,7 @@ public class DownloadUrl {
 			}
 			urlConnection.disconnect();
 		}
+		// return the Json data string.
 		return  data;
 	}
 }
